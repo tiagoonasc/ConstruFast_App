f024ddc32ce8be087be807a62e55813fc2c88ca2
Tiago Nascimento <tiagoonasc>
2025-05-27 02:01:48 -0300

    acrescentando

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:teste/src/auth/components/custom_text_field.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.red,
+              ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
+              ) ,
+              decoration: const BoxDecoration(
+              color: Colors.white,
+             borderRadius: BorderRadius.vertical(
+              top: Radius.circular(45),
+             )
+             ),
+             child: Column(
+              children:  [
+                // Email
+                CustomTextField(
+                  icon: Icons.email ,
+                  label: 'Email',
+                  ),
+               
+
+                // Senha
+                 CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                 ),
+              ],
+                ),
+              ),
+          ),
+        ],
+      ),
+        );
+  }
+}
